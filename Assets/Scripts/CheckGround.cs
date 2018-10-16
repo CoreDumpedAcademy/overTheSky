16c100a0569bd3e46c345d373be3cc0554b13072
TebionDL <purreren@gmail.com>
2018-10-16 19:13:00 +0200

    Added player movement and jump

---
+++ Assets/Scripts/CheckGround.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckGround : MonoBehaviour {
+
+    private Player_Movement player;
+    
+
+
+	// Use this for initialization
+	void Start () {
+
+        player = GetComponentInParent<Player_Movement>();
+	}
+
+     void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Floor")
+        {
+            player.grounded = true;
+        }
+    }
+
+     void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Floor")
+        {
+            player.grounded = false;
+        }
+
+        
+    }
+
+   
+}
---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+
+    
+    public float speed = 6f;
+    public float jumpPower = 6f;
+    public bool grounded;
+    
+
+    
+
+    private Rigidbody2D rb2d;
+    
+
+    // Use this for initialization
+    void Start () {
+        
+        rb2d = GetComponent<Rigidbody2D>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.Log(grounded);
+
+        float horizontal = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(horizontal * speed, rb2d.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            if (grounded == true)
+                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpPower);
+
+        }
+        
+        if (horizontal > 0.1f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        if (horizontal < -0.1f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+        
+    }
+
+     
+
+}
